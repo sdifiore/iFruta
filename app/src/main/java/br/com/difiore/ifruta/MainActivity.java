7a41afd127a4c58a89658efba854f9bd389073e7
Sergio Di Fiore <difiores@outlook.com>
2021-12-01 15:39:42 -0300

    Menu inicial

--- app/src/main/java/br/com/difiore/ifruta/MainActivity.java
+++ app/src/main/java/br/com/difiore/ifruta/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.difiore.ifruta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btProduct = (Button) findViewById(R.id.btProduct);
+        btProduct.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                System.out.println("Button Produto");
+            }
+        });
     }
 }
